93d17a084d548e47e72f7a37ef4f8a131d34b5e1
Emily Castilho Dias <emilyschool1305@gmail.com>
2026-03-11 20:08:28 -0300

    Add files via upload

---
+++ SenhaAtendimento.java
@@ -0,0 +1,18 @@
+package Static;
+
+public class SenhaAtendimento {
+    String paciente;
+    int senha;
+
+    static int controle = 100; // para ver uma variavel unica precisa do static
+
+    public SenhaAtendimento(String paciente){
+        this.paciente = paciente;
+        this.senha = controle++; //ou controle++;
+    }
+
+    public String getDados(){
+        return "Paciente: " + paciente + " | Senha # " + senha;
+    }
+
+}
