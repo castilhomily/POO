b54c678c545017b322e93cf75d6020a6107248a9
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-24 22:42:02 -0300

    finalizado

---
+++ exercicio15/Registro.java
@@ -0,0 +1,15 @@
+package exercicio15;
+
+import java.time.LocalTime;
+import java.time.Duration;
+
+public class Registro {
+    Veiculo veiculo;
+    String horaEntrada;
+    String horaSaida;
+
+    public Registro(Veiculo veiculo, String horaEntrada) {
+        this.veiculo = veiculo;
+        this.horaEntrada = horaEntrada;
+    }
+}
