9e55eb3343042391beb3d94707ff578f7f4d7276
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-01 20:14:02 -0300

    finalizado

---
+++ exercicio16/Main.java
@@ -0,0 +1,13 @@
+package exercicio16;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Produto p = new Produto("camiseta", 550);
+        System.out.println(p.getPreco());
+        p.setPreco(630);
+        System.out.println(p.getPreco());
+
+    }
+}
