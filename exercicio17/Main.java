4ac645af4df3ffa5bd7a73d6a392cc9314f06ef1
Emily Castilho Dias <emilyschool1305@gmail.com>
2026-04-13 20:20:18 -0300

    Update Main.java

--- exercicio17/Main.java
+++ exercicio17/Main.java
@@ -3,16 +3,17 @@ package exercicio17;
 public class Main {
     public static void main(String[] args) {
         Departamento departamento = new Departamento("TI01", "TI");
-        departamento.adicionarFuncionario(new Funcionario("01", "XX", "Diretor",85000));
-        departamento.adicionarFuncionario(new Funcionario("02", "YY", "Gerente",30000));
-        departamento.adicionarFuncionario(new Funcionario("03", "ZZ", "Estagiário",1500));
+        departamento.adicionarFuncionario(new Funcionario("01", "XX", "Diretor", 85000));
+        departamento.adicionarFuncionario(new Funcionario("02", "YY", "Gerente", 30000));
+        departamento.adicionarFuncionario(new Funcionario("03", "ZZ", "Estagiário", 1200));
 
-        // exibir dados do departamento
-        System.out.println(departamento.exibirRelatoio());
+        // exibir os dados do departamento
+        System.out.println(departamento.exibirRelatorio());
 
-        //deligar um funcionario
+        // desligar um funcionário
         departamento.removerFuncionario("02");
 
-        
+        // exibir os dados do departamento
+        System.out.println(departamento.exibirRelatorio());
     }
 }
