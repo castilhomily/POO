3c252bf47a08f0b23df4d7d2d4a4e33b4c1e5234
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-22 22:33:57 -0300

    finalizado

---
+++ exercicio22/Empregado.java
@@ -0,0 +1,14 @@
+package exercicio22;
+
+public abstract class Empregado extends Object {
+    long matricula;
+    String nome;
+
+    public Empregado(long matricula, String nome) {
+        this.matricula = matricula;
+        this.nome = nome;
+    }
+
+    public abstract double calcularSalario();
+
+}
